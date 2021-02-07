1462bd0bd19d2840b714d15b4aad0bb61d87857e
Romain André-Lovichi <romain.andrelovichi@gmail.com>
2021-02-07 01:50:44 +0100

    Set favicon and title

--- src/App.vue
+++ src/App.vue
@@ -7,34 +7,19 @@
         >
             <div class="d-flex align-center">
                 <v-img
-                    alt="Vuetify Logo"
+                    alt="IADC Logo"
                     class="shrink mr-2"
                     contain
-                    src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+                    src="/images/site-logo.png"
                     transition="scale-transition"
-                    width="40"
+                    width="32"
+                    heigth="32"
                 />
 
-                <v-img
-                    alt="Vuetify Name"
-                    class="shrink mt-1 hidden-sm-and-down"
-                    contain
-                    min-width="100"
-                    src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-                    width="100"
-                />
+                <h1 class="title">
+                    Imperial Assault Dice Companion
+                </h1>
             </div>
-
-            <v-spacer></v-spacer>
-
-            <v-btn
-                href="https://github.com/vuetifyjs/vuetify/releases/latest"
-                target="_blank"
-                text
-            >
-                <span class="mr-2">Latest Release</span>
-                <v-icon>mdi-open-in-new</v-icon>
-            </v-btn>
         </v-app-bar>
 
         <v-main>
@@ -58,3 +43,14 @@ export default {
     }),
 };
 </script>
+
+<style lang="scss" scoped>
+    .title {
+        font-family: "STARWARS", "Roboto" !important;
+        font-weight: normal;
+        font-size: 1.75rem !important;
+        text-transform: uppercase;
+
+        margin: 4px 0 0 10px;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
+import "./assets/css/styles.css";
+
+
 new Vue({
     vuetify,
     render: h => h(App)
